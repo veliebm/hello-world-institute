addd0e0e8ff2e817f587967b89d5a080091713df
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-09-18 19:51:57 -0400

    Add docstring header and def main

---
+++ level four_data science/project three_autocomplete/autocomplete.py
@@ -0,0 +1,15 @@
+"""
+This program uses recursion to autocomplete any word or phrase you type into it. It bases its data
+on the intrepid Reed Coke's search history.
+
+Created 9/18/2020 by Ben Velie.
+veliebm@gmail.com
+
+"""
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
