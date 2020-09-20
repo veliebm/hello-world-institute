93c837e5f4588877218b3cd39ce82d371249b310
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-09-20 11:52:24 -0400

    Add method to get child urls

--- level-4_course-dsai/project-4_name-webcrawler/crawler.py
+++ level-4_course-dsai/project-4_name-webcrawler/crawler.py
@@ -5,42 +5,60 @@ Visits all links on a website to map its structure.
 Summary
 -------
 This is a website crawler. It completely maps the input website then stores
- the structure in a file named "structure.txt".
+the structure in a file named "graph_fetus.txt".
 
 """
 
 import argparse
-import requests
 import re
 
-# This is where we will store the nodes and edges we find.
-graph_fetus_filename = "graph_fetus.txt"
+import requests
 
 
-def main():
-    # Clear the files where we'll store the urls we find.
-    open(graph_fetus_filename, "w").close()
+# This is where we will write the nodes and edges we find.
+GRAPH_FILENAME = "graph_fetus.txt"
 
-    # Get target website using argument variables.
-    parser = argparse.ArgumentParser(description="Visits all links on a website to map its structure.")
-    parser.add_argument("website", type=str, help="The website to crawl. Must start with 'https://' or 'http://'.")
-    parser.add_argument("--exclude", type=str, nargs='*', help="All the URLs you want to blacklist.")
-    arguments = parser.parse_args()
-    target_website = arguments.website
-    excluded_url_list = arguments.exclude
-    print(f"Crawling {target_website}.")
+class Crawler():
 
-    # Call recursive crawler function on the target website.
-    url_children = get_url_children(target_website, excluded_url_list)
-    print("Got URL children.")
-    output_nodes_and_edges(target_website, url_children)
+    def __init__(self, website, excluded_urls):
+        """
+        Crawls the selected website and recursively stores the structure of all child URLS.
+
+        """
+
+        pass
 
 
-def crawl(target_url, excluded_url_list):
-    url_children = get_url_children(target_url, excluded_url_list)
-    
-    
-    pass
+    def get_children(self, website: str, exclude=set()):
+        """
+        Finds all URLs on the target website.
+
+
+        Parameters
+        ----------
+        website : str
+            Target website to search.
+        exclude : iterable of strings
+            URLs to remove from the search results.
+        
+
+        Returns
+        -------
+        set
+            Websites found in the search.
+
+        """
+
+        # Store the HTML data of the target URL.
+        url_contents = requests.get(website).text
+
+        # Use a regular expression to get a list of all URLs in the HTML data.
+        regex = r"""(?<=href=[\"'])         # URL must be preceded by href= and a string beginning
+                    https?://.+?            # http and any other characters
+                    (?=[\"'])"""            # URL ends once we find a string end
+        url_list = re.findall(regex, url_contents, re.VERBOSE)
+
+        return {url for url in url_list if url not in exclude}
 
 
 def output_nodes_and_edges(parent_url: str, child_url_list: list):
@@ -53,34 +71,29 @@ def output_nodes_and_edges(parent_url: str, child_url_list: list):
         graph_fetus.write("\n")
 
 
-def exclude_urls(url_list: list, excluded_url_list: list) -> list:
-    """Returns the given URL list with the excluded URLs removed."""
-
-    trimmed_url_list = []
-    for url in url_list:
-        if excluded_url_list:
-            if url not in excluded_url_list:
-                trimmed_url_list.append(url)
-        else:
-            trimmed_url_list.append(url)
-
-    return trimmed_url_list
-
-
-def get_url_children(target_url: str, excluded_url_list: list) -> list:
-    """Outputs a list of URLs on the target page."""
-
-    # Store the HTML data of the target URL.
-    url_contents = requests.get(target_url).text
-
-    # Use a regular expression to get a list of all URLs in the HTML data.
-    regex = r"""(?<=href=[\"'])         # URL must be preceded by href= and a string beginning
-                .+?                     # Any characters
-                (?=[\"'])"""            # URL ends once we find a string end
-    url_list = re.findall(regex, url_contents, re.VERBOSE)
-
-    return url_list
-
-
 if __name__ == "__main__":
-    main()
+    """
+    Empower the user to use the program from the command line.
+
+    """
+
+    parser = argparse.ArgumentParser(description="Visits all links on a website to map its structure.")
+
+    parser.add_argument(
+        "website",
+        type=str,
+        help="The website to crawl. It must start with 'https://' or 'http://'."
+    )
+
+    parser.add_argument(
+        "--exclude",
+        "-e",
+        type=str,
+        nargs='+',
+        metavar="WEBSITE",
+        help="All the URLs you want to blacklist."
+    )
+
+    args = parser.parse_args()
+    
+    Crawler(args.website, args.exclude)
