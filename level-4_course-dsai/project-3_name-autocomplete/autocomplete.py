25c8e2e1fce4a2dfda35718c463825dc5edf8a14
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-10-29 16:49:29 -0400

    Finally make program work

--- level-4_course-dsai/project-3_name-autocomplete/autocomplete.py
+++ level-4_course-dsai/project-3_name-autocomplete/autocomplete.py
@@ -16,9 +16,13 @@ print(f"Accessing {SEARCH_PATH}...")
 
 # Access the search history.
 with open(SEARCH_PATH, "r", encoding="UTF-8") as search_file:
-    contents = search_file.read()
-    phrase_list = [phrase.split(" ") for phrase in contents.splitlines()]
-    word_list = contents.replace("\n", "").split(" ")
+    contents = search_file.read().lower()
+
+    for character in "!@#$%^&*(){}:\",.?<>/;[]\\-_=+":
+        contents = contents.replace(character, "")
+
+    phrase_list = [phrase.split() for phrase in contents.splitlines()]
+    word_list = contents.split()
 
 # Store the search history in Tries.
 PHRASE_TRIE = Trie("!!PHRASES!!")
@@ -34,11 +38,21 @@ for word in word_list:
 def main():
     mode = int(input("Welcome to Ben's autocompleter!\nEnter 1 for word mode or 2 for phrase mode.\n>> "))
     
-    if mode == 1:
-        input_word = input("Enter a part of a word. I'll finish it.\n>> ")
+    while True:
+        if mode == 1:
+            input_word = input("Enter a part of a word. I'll finish it.\n>> ")
+            rest_of_the_word = "".join((WORD_TRIE.unstore(input_word).autocomplete()))
+            full_word = input_word + rest_of_the_word
+            print(full_word)
 
-    if mode == 2:
-        input_phrase = input("Enter part of a phrase. I'll finish it.\n>> ")
+        elif mode == 2:
+            input_phrase = input("Enter part of a phrase. I'll finish it.\n>> ")
+            rest_of_the_phrase = " ".join((PHRASE_TRIE.unstore([input_phrase]).autocomplete()))
+            full_phrase = input_phrase + " " + rest_of_the_phrase
+            print(full_phrase)
+
+        else:
+            raise RuntimeError("Whoah there! You can only pick 1 or 2!")
 
 
 if __name__ == "__main__":
